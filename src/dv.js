fd878cac0c53abe4f89dde7b135ace8601320ca0
Loduis Madariaga <loduis@gmail.com>
2024-10-04 08:09:00 -0500

    refactor: ruc table.

--- src/dv.js
+++ src/dv.js
@@ -2,7 +2,6 @@ const NATURAL = 1
 const LEGAL = 2
 
 const N = '5'
-const NT = '43'
 
 const LETTERS = {
   E: { code: '5', validate: '66' },
@@ -77,11 +76,15 @@ const calculate = (sw, ructb) => {
 }
 
 class DV {
+  #type
+  #len
+  #isOld
+  #ruc
   constructor (ruc, type) {
-    this.type = +type
-    this.ruc = ruc.toUpperCase().split('-')
-    this.len = this.ruc.length
-    this.isOld = false
+    this.#type = +type
+    this.#ruc = ruc.toUpperCase().split('-')
+    this.#len = this.#ruc.length
+    this.#isOld = false
   }
 
   static natural (ruc) {
@@ -96,124 +99,60 @@ class DV {
     return new this(ruc, type).get()
   }
 
-  get isNatural () {
-    return this.type === NATURAL
+  get #isNatural () {
+    return this.#type === NATURAL
   }
 
-  get isLegal () {
-    return this.type !== NATURAL
+  get #isLegal () {
+    return this.#type !== NATURAL
   }
 
-  get rucTable () {
-    if (this.len < 3) return null
-    if (this.len === 4) {
-      if (this.ruc[1] !== 'NT') return null
-      const [province, , imageFolio, seatToken] = this.ruc
-      if (
-        isNaN(province) ||
-        isNaN(imageFolio) ||
-        isNaN(seatToken) ||
-        province.length > 2 ||
-        imageFolio.length > 3 ||
-        (this.isNatural && seatToken.length > 6) ||
-        (this.isLegal && seatToken.length > 7)
-      ) {
-        return null
-      }
-      let len = seatToken.length
-      const isSix = len === 6
-      len = isSix ? 6 : 5
-      return (
-        zFill(this.isNatural ? N : '0', isSix ? 7 : 8) +
-        zFill(province, 2) +
-        NT +
-        zFill(imageFolio, 3) +
-        zFill(seatToken.slice(0, len), len)
-      )
+  get #rucTable () {
+    if (this.#len < 3) return null
+    if (this.#len === 4) {
+      const [province, key, imageFolio, seatToken] = this.#ruc
+      return key === 'NT' ? this.#rtNt(province, imageFolio, seatToken) : null
     }
 
-    let [letter, imageFolio, seatToken] = this.ruc
-    let ructb = N
+    const [letter, imageFolio, seatToken] = this.#ruc
+
     if (['AV', 'PI'].some((lt) => letter.endsWith(lt))) {
-      const province = letter.slice(0, -2)
-      if (
-        isNaN(province) ||
-        isNaN(imageFolio) ||
-        isNaN(seatToken) ||
-        province.length > 2 ||
-        imageFolio.length > 4 ||
-        seatToken.length > 8
-      ) {
-        return null
-      }
-      letter = letter.slice(-2)
-      const info = LETTERS[letter]
-      if (imageFolio.length < 4) {
-        ructb += zFill(province, 2) + info.code + zFill(imageFolio, 3)
-      } else {
-        ructb += info.validate + zFill(imageFolio, 4)
-      }
-
-      ructb += zFill(seatToken.slice(0, 5), 5)
-      return zFill(ructb, 20)
+      return this.#rtAvPi(letter, imageFolio, seatToken)
     }
 
-    if (['E', 'PE', 'N'].includes(letter)) {
-      if (
-        isNaN(imageFolio) ||
-        isNaN(seatToken) ||
-        imageFolio.length > 4 ||
-        seatToken.length > 9
-      ) {
-        return null
-      }
-      let len = 5
-      const info = LETTERS[letter]
-      if (seatToken.length === 6 && ['E', 'N'].includes(letter)) {
-        len++
-      }
-      const isSix = len === 6
-      if (imageFolio.length < 4) {
-        ructb +=
-          '00' +
-          zFill(info.code, 2, false) +
-          zFill(imageFolio, 3) +
-          zFill(seatToken.slice(0, len), len)
-      } else {
-        ructb +=
-          info.validate +
-          (isSix ? zFill(info.code, 2, false) : info.code) +
-          zFill(imageFolio, 4) +
-          zFill(seatToken.slice(0, len), len)
-      }
-      return zFill(ructb, 20)
+    if (['N', 'E', 'PE'].includes(letter)) {
+      return this.#rtNePe(letter, imageFolio, seatToken)
     }
 
     if (isNaN(letter)) return null
 
-    if (this.isNatural) {
-      const province = letter
-      if (
-        isNaN(province) ||
-        isNaN(imageFolio) ||
-        isNaN(seatToken) ||
-        province.length > 2 ||
-        imageFolio.length > 4 ||
-        seatToken.length > 9
-      ) {
-        return null
-      }
-      return zFill(
-        ructb +
+    return this.#isNatural
+      ? this.#rtEmptyLetterNatural(letter, imageFolio, seatToken)
+      : this.#rtEmptyLetterLegal(letter, imageFolio, seatToken)
+  }
+
+  #rtEmptyLetterNatural (province, imageFolio, seatToken) {
+    if (
+      isNaN(province) ||
+      isNaN(imageFolio) ||
+      isNaN(seatToken) ||
+      province.length > 2 ||
+      imageFolio.length > 4 ||
+      seatToken.length > 9
+    ) {
+      return null
+    }
+    return zFill(
+      N +
           zFill(province, 2) +
           '00' +
           zFill(imageFolio, 3) +
           zFill(seatToken.slice(0, 5), 5),
-        20
-      )
-    }
+      20
+    )
+  }
 
-    const rollTome = letter
+  #rtEmptyLetterLegal (rollTome, imageFolio, seatToken) {
     if (
       isNaN(rollTome) ||
       isNaN(imageFolio) ||
@@ -225,10 +164,10 @@ class DV {
       return null
     }
 
-    ructb =
+    let ructb =
       zFill(rollTome, 10) + zFill(imageFolio, 4) + zFill(seatToken, 6)
     if (ructb[3] === '0' && ructb[4] === '0' && +ructb[5] < 5) {
-      this.isOld = true
+      this.#isOld = true
       ructb =
         ructb.slice(0, 5) +
         (OLD_RUC_CROSS_REF[ructb.slice(5, 7)] || ructb.slice(5, 7)) +
@@ -237,11 +176,91 @@ class DV {
     return ructb
   }
 
+  #rtNt (province, imageFolio, seatToken) {
+    if (
+      isNaN(province) ||
+      isNaN(imageFolio) ||
+      isNaN(seatToken) ||
+      province.length > 2 ||
+      imageFolio.length > 3 ||
+      (this.#isNatural && seatToken.length > 6) ||
+      (this.#isLegal && seatToken.length > 7)
+    ) {
+      return null
+    }
+    let len = seatToken.length
+    const isSix = len === 6
+    len = isSix ? 6 : 5
+    return (
+      zFill(this.#isNatural ? N : '0', isSix ? 7 : 8) +
+      zFill(province, 2) +
+      '43' +
+      zFill(imageFolio, 3) +
+      zFill(seatToken.slice(0, len), len)
+    )
+  }
+
+  #rtAvPi (letter, imageFolio, seatToken) {
+    const province = letter.slice(0, -2)
+    if (
+      isNaN(province) ||
+      isNaN(imageFolio) ||
+      isNaN(seatToken) ||
+      province.length > 2 ||
+      imageFolio.length > 4 ||
+      seatToken.length > 8
+    ) {
+      return null
+    }
+    letter = letter.slice(-2)
+    const info = LETTERS[letter]
+    let ructb = N
+    if (imageFolio.length < 4) {
+      ructb += zFill(province, 2) + info.code + zFill(imageFolio, 3)
+    } else {
+      ructb += info.validate + zFill(imageFolio, 4)
+    }
+    ructb += zFill(seatToken.slice(0, 5), 5)
+    return zFill(ructb, 20)
+  }
+
+  #rtNePe (letter, imageFolio, seatToken) {
+    if (
+      isNaN(imageFolio) ||
+      isNaN(seatToken) ||
+      imageFolio.length > 4 ||
+      seatToken.length > 9
+    ) {
+      return null
+    }
+    let len = 5
+    const info = LETTERS[letter]
+    if (seatToken.length === 6 && ['E', 'N'].includes(letter)) {
+      len++
+    }
+    const isSix = len === 6
+    let ructb = N
+    if (imageFolio.length < 4) {
+      ructb +=
+        '00' +
+        zFill(info.code, 2, false) +
+        zFill(imageFolio, 3) +
+        zFill(seatToken.slice(0, len), len)
+    } else {
+      ructb +=
+        info.validate +
+        (isSix ? zFill(info.code, 2, false) : info.code) +
+        zFill(imageFolio, 4) +
+        zFill(seatToken.slice(0, len), len)
+    }
+    return zFill(ructb, 20)
+  }
+
   get () {
-    const ructb = this.rucTable
+    const ructb = this.#rucTable
     if (!ructb) return null
-    const dv1 = calculate(this.isOld, ructb)
-    const dv2 = calculate(this.isOld, ructb + dv1)
+    const dv1 = calculate(this.#isOld, ructb)
+    const dv2 = calculate(this.#isOld, ructb + dv1)
 
     return dv1 * 10 + dv2
   }
